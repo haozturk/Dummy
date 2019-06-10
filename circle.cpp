935144a0a147e5b1aed88ad496c69b4926b6bae1
Hasan Öztürk <32415369+hasan-ozturk-2017400258@users.noreply.github.com>
2019-06-10 23:15:54 +0300

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+#define PI 3.14159265359
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+    double n,r;
+    cin >> n >> r;
+
+    double angle = 180/n;
+    
+    cout << (r*sin(angle*PI/180))/(1-sin(angle*PI/180)) << endl;
+   
+    return 0;
+}
